81d8c6f3f67318a760ba8282a11d9958671f80af
Darren Whatford <darrenwhatford@hastingsdirect.com>
2019-08-09 07:16:56 +0100

    Added initial script.js file
    Added initial script.js file for training

---
+++ script.js
@@ -0,0 +1,91 @@
+/*var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'teacher';
+console.log(job);*/
+
+//One line of comments
+
+/*
+Multi lines of comments or commented out code
+*/
+
+/*
+Type coercion 
+Type coercion is the process of converting value from one type to another (such as string to number, object to boolean, and so on)
+boolean == integer
+the boolean operand will be converted to an integer: false becomes 0, true becomes 1. Then the two values are compared.
+*/
+
+/*var firstName = 'John';
+var age = 28;
+console.log (firstName + ' ' + age);
+
+var job, isMarried;
+
+job = 'teacher';
+isMarried = false;
+
+console.log (firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation - change the value of a variable
+age = 'twenty eight';
+job = 'driver';
+
+alert (firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt ('What is your last name?');
+console.log (firstName + ' ' + lastName);*/
+
+//Basic operators
+
+var now, ageJohn, ageMark, yearJohn, yearMark;
+now = 2019;
+ageJohn = 28
+ageMark = 49
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log (yearJohn);
+console.log (yearMark);
+
+console.log (now + 1);
+
+// Math operators: +, -, *, /,
+
+// Logical operators <, >,
+
+var older = ageJohn > ageMark;
+console.log (older);
+
+// typeof operator
+console.log (typeof older);
+console.log (typeof ageJohn);
+console.log (typeof 'Mark is older than John');
+var abc;
+console.log (typeof x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
